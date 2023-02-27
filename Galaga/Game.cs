3f458522dd733137487cc22e22d2d4fb28fa329f
Hallgrimurth <hallgrimur1998@gmail.com>
2023-02-27 11:58:09 +0100

    adding basic player movement

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -4,36 +4,85 @@ using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 using DIKUArcade;
 using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
+using System.Collections.Generic;
 
 namespace Galaga
 {
-    public class Game : DIKUGame
+    public class Game : DIKUGame, IGameEventProcessor
     {
+        private GameEventBus eventBus;
         private Player player;
         public Game(WindowArgs windowArgs) : base(windowArgs) {
+
+            eventBus = new GameEventBus();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+            window.SetKeyEventHandler(KeyHandler);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+
+
             player = new Player(
                 new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
                 new Image(Path.Combine("Assets", "Images", "Player.png")));
+        
         }
             // TODO: Set key event handler (inherited window field of DIKUGame class)
         
+        private void KeyPress(KeyboardKey key) {
+            // TODO: Close window if escape is pressed
+            // TODO: switch on key string and set the player's move direction
+            switch (key) {
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(true);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(true);
+                    break;
+                case KeyboardKey.Escape:
+                    window.CloseWindow();
+                    break;
+            }
+        }
+        private void KeyRelease(KeyboardKey key) {
+            // TODO: switch on key string and disable the player's move direction
+            switch (key) {
+                case KeyboardKey.Left:
+                    player.SetMoveLeft(false);
+                    break;
+                case KeyboardKey.Right:
+                    player.SetMoveRight(false);
+                    break;
+            }
+        }
 
-        //private void KeyHandler(KeyboardAction action, KeyboardKey key) {} // TODO: Outcomment
+        private void KeyHandler(KeyboardAction action, KeyboardKey key) {
+            switch (action) {
+                case KeyboardAction.KeyPress:
+                    KeyPress(key);
+                    break;
+                case KeyboardAction.KeyRelease:
+                    KeyRelease(key);
+                    break;
+            }
+        } // TODO: Outcomment
+
+        public void ProcessEvent(GameEvent gameEvent) {
+            // Leave this empty for now
+        }
+
 
         public override void Render()
         {
             //TODO: Render Game Entities
-            window.Clear();
+            // window.Clear();
             player.Render();
-
-
-            // throw new System.NotImplementedException("Galaga game has nothing to render yet.");
         }
 
         public override void Update()
         {
             window.PollEvents();
-            // throw new System.NotImplementedException("Galaga game has no entities to update yet.");
+            player.Move();
         }
     }
 }
