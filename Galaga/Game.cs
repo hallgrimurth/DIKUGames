b95ad959824918dc09e11cabb278b30c0fefc712
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-14 16:30:18 +0100

    test

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -142,17 +142,20 @@ namespace Galaga
         private void KeyPress(KeyboardKey key) {
             switch(key) {
                 case KeyboardKey.Left:
-                    // eventBus.RegisterEvent(new GameEvent{EventType = GameEventType.MovementEvent, From = this, To = player, Message = "MOVE_LEFT" });
-                    player.SetMoveLeft(true);
+                    GameEvent MoveLeft = (new GameEvent{EventType = GameEventType.MovementEvent, From = this, To = player, Message = "MOVE_LEFT" });
+                    eventBus.RegisterEvent(MoveLeft);
                     break;
                 case KeyboardKey.Right:
-                    player.SetMoveRight(true);
+                    GameEvent MoveRight = (new GameEvent{EventType = GameEventType.MovementEvent, From = this, To = player, Message = "MOVE_RIGHT" });
+                    eventBus.RegisterEvent(MoveRight);
                     break;
                 case KeyboardKey.Up:
-                    player.SetMoveUp(true);
+                    GameEvent MoveUp = (new GameEvent{EventType = GameEventType.MovementEvent, From = this, To = player, Message = "MOVE_UP" });
+                    eventBus.RegisterEvent(MoveUp);
                     break;
                 case KeyboardKey.Down:
-                    player.SetMoveDown(true);
+                     GameEvent MoveDown = (new GameEvent{EventType = GameEventType.MovementEvent, From = this, To = player, Message = "MOVE_DOWN" });
+                    eventBus.RegisterEvent(MoveDown);
                     break;
                 case KeyboardKey.Escape:
                     GameEvent CloseWindowEvent = (new GameEvent {EventType = GameEventType.WindowEvent, From = this, Message = "CLOSE_WINDOW"});
@@ -204,23 +207,23 @@ namespace Galaga
                     break;
                 }
             }
-            // else if (gameEvent.EventType == GameEventType.MovementEvent) {
-            //     switch(gameEvent.Message){
-            //         case "MOVE_LEFT":
-            //             player.SetMoveLeft(true);
-            //             break;
-            //         case "MOVE_RIGHT":
-            //             player.SetMoveRight(true);
-            //             break;
-            //         case "MOVE_UP":
-            //             player.SetMoveUp(true);
-            //             break;
-            //         case "MOVE_DOWN":
-            //             player.SetMoveDown(true);
-            //             break;
-            //     }
+            else if (gameEvent.EventType == GameEventType.MovementEvent) {
+                switch(gameEvent.Message){
+                    case "MOVE_LEFT":
+                        player.SetMoveLeft(true);
+                        break;
+                    case "MOVE_RIGHT":
+                        player.SetMoveRight(true);
+                        break;
+                    case "MOVE_UP":
+                        player.SetMoveUp(true);
+                        break;
+                    case "MOVE_DOWN":
+                        player.SetMoveDown(true);
+                        break;
+                 }
                 
-            // }
+            }
         }
             
             
