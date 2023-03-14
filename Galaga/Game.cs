c6bde263f05572816ac16fb57e79dc93d0b351a5
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-14 15:10:56 +0100

    adding HP and level score

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -17,6 +17,7 @@ namespace Galaga
     public class Game : DIKUGame, IGameEventProcessor {
         private GameEventBus eventBus;
         private Player player;
+        private Health health;
         private EntityContainer<Enemy> enemies;
         private List<Image> enemyStridesRed;
         private int numEnemies = 8;
@@ -38,6 +39,11 @@ namespace Galaga
             eventBus.Subscribe(GameEventType.PlayerEvent, this);
             eventBus.Subscribe(GameEventType.MovementEvent, this);
 
+            //Adding Health
+            health = new Health( new Vec2F(0.75f, -0.2f), new Vec2F(0.4f, 0.4f));
+            // Adding score
+            score = new Score(new Vec2F(0.75f, -0.3f), new Vec2F(0.4f, 0.4f));
+
             // Adding player
             List<Image> playerimages = ImageStride.CreateStrides
                 (4, Path.Combine("Assets", "Images", "FlightAnimation.png"));
@@ -46,7 +52,7 @@ namespace Galaga
                 new ImageStride(160, playerimages));
 
             // Adding Enemies
-            List<Image> enemyStridesRed = ImageStride.CreateStrides
+            enemyStridesRed = ImageStride.CreateStrides
                 (2, Path.Combine("Assets", "Images", "RedMonster.png"));
             List<Image> BlueMonster = ImageStride.CreateStrides
                 (4, Path.Combine("Assets", "Images", "BlueMonster.png"));
@@ -55,7 +61,7 @@ namespace Galaga
             for (int i = 0; i < numEnemies; i++) {
                 enemies.AddEntity(new Enemy(
                     new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
-                    new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
+                    new ImageStride(80, BlueMonster), new ImageStride(40, enemyStridesRed)));
             }
       
             // Adding shooting functionality
@@ -67,8 +73,7 @@ namespace Galaga
             explosionStrides = ImageStride.CreateStrides(8,
                 Path.Combine("Assets", "Images", "Explosion.png"));
 
-            // Adding score
-            score = new Score();
+            
         }
 
         // Check for collisions and delete entities if they collide - also add point to score
@@ -89,7 +94,7 @@ namespace Galaga
                             if (enemy.Hitpoints == 0) {
                                 enemy.DeleteEntity();
                                 AddExplosion(enemy.Shape.Position, enemy.Shape.Extent);
-                                score.AddPoint();         
+                                // score.AddPoint();         
                             }
                             //change monster to red when hitpoints are low
                             if (enemy.Hitpoints == 1) {
@@ -107,12 +112,13 @@ namespace Galaga
                 (4, Path.Combine("Assets", "Images", "BlueMonster.png"));
 
             if (enemies.CountEntities() == 0) {
+                score.AddPoint();
                 enemySpeed += 0.0005f;
                 for (int i = 0; i < numEnemies; i++) {
                     enemies.AddEntity(new Enemy(
                         new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 1.0f), 
                             new Vec2F(0.1f, 0.1f)),
-                        new ImageStride(80, images), new ImageStride(80, enemyStridesRed)));
+                        new ImageStride(80, images), new ImageStride(40, enemyStridesRed)));
                 }
             }
         }
@@ -203,34 +209,19 @@ namespace Galaga
                     break;
                 }
             }
-            // else if (gameEvent.EventType == GameEventType.MovementEvent) {
-            //     switch(gameEvent.Message){
-            //         case "MOVE_LEFT":
-            //             player.SetMoveLeft(true);
-            //             break;
-            //         case "MOVE_RIGHT":
-            //             player.SetMoveRight(true);
-            //             break;
-            //         case "MOVE_UP":
-            //             player.SetMoveUp(true);
-            //             break;
-            //         case "MOVE_DOWN":
-            //             player.SetMoveDown(true);
-            //             break;
-            //     }
-                
-            // }
         }
             
             
 
         public override void Render() {
             window.Clear();
+            health.RenderHealth();
             player.Render();
             playerShots.RenderEntities();
             enemies.RenderEntities();
             enemyExplosions.RenderAnimations();
             score.Render();
+
         }
 
         public override void Update() {
