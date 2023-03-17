e3404cb12b4ee6fd1853ca0e415270c68971c781
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 12:10:18 +0100

    using_startpos_from_enemy

--- Galaga/MovementStrategies/ZigZagDown.cs
+++ Galaga/MovementStrategies/ZigZagDown.cs
@@ -8,9 +8,8 @@ namespace Galaga.MovementStrategy {
         public float p = 0.045; 
         public float a = 0.05;  
         public void MoveEnemy (Enemy enemy){
-        
-        
-
+            shape.Position.Y = startPos[1] + s;
+            shape.Position.X = startPos[0] + a*(Math.Sin(shape.Position.Y-startPos[1])/p);
         }
 
         public void MoveEnemies (EntityContainer<Enemy> enemies){
