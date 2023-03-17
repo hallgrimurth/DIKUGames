e56fb82eeaf89ec684900bb66e37d01d3838f4a6
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 13:15:46 +0100

    down

--- Galaga/MovementStrategies/Down.cs
+++ Galaga/MovementStrategies/Down.cs
@@ -11,6 +11,8 @@ namespace Galaga.MovementStrategy {
         // // Moving enemies down at random speeds and deleting them if they are out of bounds
         // // Also resetting score and enemy speed if enemy is out of bounds
         public void MoveEnemy (Enemy enemy){
+            float enemySpeed = Galaga.Game.EnemySpeed;
+            Random rand = new Random(); 
             float speed = enemySpeed + rand.Next(1, 100) / 25000.0f;
             enemy.Shape.MoveY(-speed);
             if (enemy.Shape.Position.Y < -0.1f) {
