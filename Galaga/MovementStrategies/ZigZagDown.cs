a631ab3fcc89006498ebe32282f95cc2474a0a19
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 14:00:08 +0100

    zigzagdown

--- Galaga/MovementStrategies/ZigZagDown.cs
+++ Galaga/MovementStrategies/ZigZagDown.cs
@@ -1,22 +1,33 @@
-using System;
+using System.IO;
 using DIKUArcade.Entities;
-using Galaga.MovementStrategy;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
+using System.Collections.Generic;
+using DIKUArcade.Physics;
+using System;
 
 namespace Galaga.MovementStrategy {
     public class ZigZagDown : IMovementStrategy {
-        public float s = 0.0003; 
-        public float p = 0.045; 
-        public float a = 0.05; 
+        public float s = 0.0003f; 
+        public float p = 0.045f; 
+        public float a = 0.05f; 
         public DynamicShape shape;
 
         
         public void MoveEnemy (Enemy enemy){
-            shape.Position.Y = Galaga.Enemy.startPos[1] + s;
-            shape.Position.X = Galaga.Enemy.startPos[0] + a*(Math.Sin(shape.Position.Y-startPos[1])/p);
+            float startPosY = enemy.startPos.Y;
+            float startPosX = enemy.startPos.X;
+            float y = startPosY + s;
+            float x = startPosX + a*(float)Math.Sin(shape.Position.Y-startPosY)/p;
+            enemy.shape.SetPosition(new Vec2F(x,y));
         }
 
         public void MoveEnemies (EntityContainer<Enemy> enemies){
-            foreach(Enemy enemy in EntityContainer<Enemy> enemies){
+            foreach(Enemy enemy in enemies){
                 MoveEnemy(enemy);
             }
         }
