c13dc81b318cb473783cac90e31fac8838788159
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 11:42:39 +0100

    squadron

--- Galaga/Squadron.cs
+++
@@ -1,8 +0,0 @@
-namespace Galaga.Squadron {
-public interface ISquadron {
-    EntityContainer<Enemy> Enemies {get;}
-    int MaxEnemies {get;}
-    void CreateEnemies (List<Image> enemyStride,
-        List<Image> alternativeEnemyStride);
-    }
-}
