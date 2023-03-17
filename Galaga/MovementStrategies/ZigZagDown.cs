8c1da5a79f42408d9b0f3761e81de03ca5ed0c30
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 13:16:35 +0100

    zigzag

--- Galaga/MovementStrategies/ZigZagDown.cs
+++ Galaga/MovementStrategies/ZigZagDown.cs
@@ -6,10 +6,13 @@ namespace Galaga.MovementStrategy {
     public class ZigZagDown : IMovementStrategy {
         public float s = 0.0003; 
         public float p = 0.045; 
-        public float a = 0.05;  
+        public float a = 0.05; 
+        public DynamicShape shape;
+
+        
         public void MoveEnemy (Enemy enemy){
-            shape.Position.Y = startPos[1] + s;
-            shape.Position.X = startPos[0] + a*(Math.Sin(shape.Position.Y-startPos[1])/p);
+            shape.Position.Y = Galaga.Enemy.startPos[1] + s;
+            shape.Position.X = Galaga.Enemy.startPos[0] + a*(Math.Sin(shape.Position.Y-startPos[1])/p);
         }
 
         public void MoveEnemies (EntityContainer<Enemy> enemies){
