7eabdf9eef4b79b0a336a71fa32b7ff6d00b19d0
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 13:16:52 +0100

    circle

--- Galaga/Squadron/Circle.cs
+++ Galaga/Squadron/Circle.cs
@@ -1,17 +1,29 @@
-using System;
+using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
 using System.Collections.Generic;
+using DIKUArcade.Physics;
+using System;
 
 namespace Galaga.Squadron {
     public class Circle : ISquadron {
         public EntityContainer<Enemy> Enemies {get;}
         public int MaxEnemies {get;}
+    
+        public  Random rand = new Random(); // For randomizing enemy speed
+
         public void CreateEnemies (List<Image> enemyStride, List<Image> alternativeEnemyStride){
+            List<Image> enemyStridesRed = ImageStride.CreateStrides(2, Path.Combine("Assets", "Images", "RedMonster.png"));
+            List<Image> enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("Assets", "Images", "BlueMonster.png"));
             // enemies in a circle
-            for (int i = 0; i < numEnemies; i++) {
-                enemies.AddEntity(new Enemy(
-                    new DynamicShape(new Vec2F(0.45f + (float)Math.Cos(i*2*Math.PI/numEnemies)*0.2f, 0.6f + (float)Math.Sin(i*2*Math.PI/numEnemies)*0.2f), new Vec2F(0.1f, 0.1f)),
+            for (int i = 0; i < MaxEnemies; i++) {
+                Enemies.AddEntity(new Enemy(
+                    new DynamicShape(new Vec2F(0.45f + (float)Math.Cos(i*2*Math.PI/this.MaxEnemies)*0.2f, 0.6f + (float)Math.Sin(i*2*Math.PI/this.MaxEnemies)*0.2f), new Vec2F(0.1f, 0.1f)),
                     new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
             }
 
