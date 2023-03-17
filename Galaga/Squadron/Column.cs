d4701775fa27891cc1126d20497fc16650016e3f
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 18:32:06 +0100

    minor_changes

--- Galaga/Squadron/Column.cs
+++ Galaga/Squadron/Column.cs
@@ -11,16 +11,16 @@ using DIKUArcade.Physics;
 using System;
 
 namespace Galaga.Squadron {
-    public class Wave : ISquadron {
+    public class Column : ISquadron {
         public EntityContainer<Enemy> Enemies {get;}
         public int MaxEnemies {get;}
          public  Random rand = new Random(); // For randomizing enemy speed
         public void CreateEnemies (List<Image> enemyStride, List<Image> alternativeEnemyStride){
-            // wave pattern
-            for (int i = 0; i < MaxEnemies; i++) {
-                Enemies.AddEntity(new Enemy(
-                    new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, (0.9f - (float)i*0.03f)), new Vec2F(0.1f, 0.1f)),
-                    new ImageStride(80, enemyStride), new ImageStride(80, alternativeEnemyStride)));
+            // enemies in columns on the left and right
+            for (int i = 0; i < MaxEnemies/2; i++) {
+                enemies.AddEntity(new Enemy(
+                    new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
+                    new ImageStride(80, enemyStride), new ImageStride(40, alternativeEnemyStride)));
             }
         }
     }
