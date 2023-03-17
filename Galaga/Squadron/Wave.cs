ad68535cabe7eb0d769285709358d6fd46b6c406
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-17 14:02:18 +0100

    zigzag

--- Galaga/Squadron/Wave.cs
+++ Galaga/Squadron/Wave.cs
@@ -1,18 +1,28 @@
-using System;
+using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
 using System.Collections.Generic;
+using DIKUArcade.Physics;
+using System;
 
 namespace Galaga.Squadron {
     public class Wave : ISquadron {
         public EntityContainer<Enemy> Enemies {get;}
         public int MaxEnemies {get;}
+         public  Random rand = new Random(); // For randomizing enemy speed
         public void CreateEnemies (List<Image> enemyStride, List<Image> alternativeEnemyStride){
+            List<Image> enemyStridesRed = ImageStride.CreateStrides(2, Path.Combine("Assets", "Images", "RedMonster.png"));
+            List<Image> enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("Assets", "Images", "BlueMonster.png"));
             // wave pattern
-            for (int i = 0; i < numEnemies; i++) {
+            for (int i = 0; i < MaxEnemies; i++) {
                 Enemies.AddEntity(new Enemy(
                     new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, (0.9f - (float)i*0.03f)), new Vec2F(0.1f, 0.1f)),
-                    new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
+                    new ImageStride(80, enemyStridesBlue), new ImageStride(80, enemyStridesRed)));
             }
         }
     }
--- Galaga/Squadron/ZigZag.cs
+++ Galaga/Squadron/ZigZag.cs
@@ -1,56 +1,31 @@
-using System;
+using System.IO;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Events;
+using DIKUArcade.Input;
 using System.Collections.Generic;
+using DIKUArcade.Physics;
+using System;
 
 namespace Galaga.Squadron {
     public class ZigZag : ISquadron {
         public EntityContainer<Enemy> Enemies {get;}
         public int MaxEnemies {get;}
+         public  Random rand = new Random(); // For randomizing enemy speed
         public void CreateEnemies (List<Image> enemyStride,List<Image> alternativeEnemyStride){
-                // zigzag pattern
+            List<Image> enemyStridesRed = ImageStride.CreateStrides(2, Path.Combine("Assets", "Images", "RedMonster.png"));
+            List<Image> enemyStridesBlue = ImageStride.CreateStrides(4, Path.Combine("Assets", "Images", "BlueMonster.png"));
+            
+            // zigzag pattern
             for (int i = 0; i < MaxEnemies; i++) {
                 Enemies.AddEntity(new Enemy(
                     new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, (0.9f - ((float)i*0.1f%0.2f))), new Vec2F(0.1f, 0.1f)),
-                    new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
+                    new ImageStride(80, enemyStridesBlue), new ImageStride(80, enemyStridesRed)));
             }
 
         }
     }
 }
-
-        // void ZigZagFormation(){
-        //     enemies = new CreateEnemies (List<Image> enemyStride,
-        //     List<Image> alternativeEnemyStride);
-            //new EntityContainer<Enemy>(numEnemies);
-            // zigzag pattern
-                // for (int i = 0; i < MaxEnemies; i++) {
-                //  enemies.AddEntity(new Enemy(
-                        // new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, (0.9f - ((float)i*0.1f%0.2f))), new Vec2F(0.1f, 0.1f)),
-                        // new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
-                // }
-        // }
-            
-            
-        
-            // normal enemies in a row
-            //for (int i = 0; i < numEnemies; i++) {
-            //     enemies.AddEntity(new Enemy(
-            //         new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, 0.9f), new Vec2F(0.1f, 0.1f)),
-            //         new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
-            // }
-        
-
-            // // wave pattern
-            // for (int i = 0; i < numEnemies; i++) {
-            //     enemies.AddEntity(new Enemy(
-            //         new DynamicShape(new Vec2F(0.1f + (float)i * 0.1f, (0.9f - (float)i*0.03f)), new Vec2F(0.1f, 0.1f)),
-            //         new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
-            // }
-
-            // // enemies in a circle
-            // for (int i = 0; i < numEnemies; i++) {
-            //     enemies.AddEntity(new Enemy(
-            //         new DynamicShape(new Vec2F(0.45f + (float)Math.Cos(i*2*Math.PI/numEnemies)*0.2f, 0.6f + (float)Math.Sin(i*2*Math.PI/numEnemies)*0.2f), new Vec2F(0.1f, 0.1f)),
-            //         new ImageStride(80, BlueMonster), new ImageStride(80, enemyStridesRed)));
-            // }
