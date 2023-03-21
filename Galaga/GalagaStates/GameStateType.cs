1ae1ab0d90d4efaad5f8bdb066df418304ead1d9
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-21 14:39:56 +0100

    moveing galagastatetest

--- Galaga/GalagaStates/GameStateType.cs
+++ Galaga/GalagaStates/GameStateType.cs
@@ -1,46 +1,4 @@
 using System;
-<<<<<<< HEAD
-enum GameStateType{
-    GameRunning,
-    GamePaused,
-    GameOver,
-    MainMenu
-}
-    
-class StateTransformer{
-
-    public static GameStateType TransformStringToState(string state){
-        switch(state){
-            case "GAME RUNNING":
-                return GameStateType.GameRunning;
-            case "GAME PAUSED":
-                return GameStateType.GamePaused;
-            case "GAME OVER":
-                return GameStateType.GameOver;
-            case "MAIN MENU":
-                return GameStateType.MainMenu;
-            default:
-                throw(new ArgumentException());
-        }
-    }
-
-    public static string TransformStateToString(GameStateType state){
-        switch(state){
-            case GameStateType.GameRunning:
-                return "GAME RUNNING";
-            case GameStateType.GamePaused:
-                return "GAME PAUSED";
-            case GameStateType.GameOver:
-                return "GAME OVER";
-            case GameStateType.MainMenu:
-                return "MAIN MENU";
-            default:
-                throw(new ArgumentException());
-        }
-    }
-    
-} 
-=======
 
 namespace Galaga.GalagaStates {
     public enum GameStateType {
@@ -84,7 +42,6 @@ namespace Galaga.GalagaStates {
         }
     } 
 }
->>>>>>> halli
 
 
  
