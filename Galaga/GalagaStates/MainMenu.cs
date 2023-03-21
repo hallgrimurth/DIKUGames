52783fa2a63ab92eb763237b489d702a8a86c605
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-03-21 12:57:51 +0100

    newclasses

---
+++ Galaga/GalagaStates/MainMenu.cs
@@ -0,0 +1,16 @@
+namespace Galaga.GalagaStates {
+    public class MainMenu : IGameState {
+        private static MainMenu instance = null;
+        private Entity backGroundImage;
+        private Text[] menuButtons;
+        private int activeMenuButton;
+        private int maxMenuButtons;
+        public static MainMenu GetInstance() {
+            if (MainMenu.instance == null) {
+                MainMenu.instance = new MainMenu();
+                MainMenu.instance.InitializeGameState();
+            }
+            return MainMenu.instance;
+        }
+    }
+}
