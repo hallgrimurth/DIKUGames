fe15ece96cbcd6a0dcdec6574e0c069482f8e92d
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-21 15:08:33 +0100

    mainmenustate start

--- Galaga/GalagaStates/MainMenu.cs
+++ Galaga/GalagaStates/MainMenu.cs
@@ -1,12 +1,20 @@
-//using DIKUArcade.Graphics.Text;
+using DIKUArcade.State;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 
 namespace Galaga.GalagaStates {
     public class MainMenu : IGameState {
         private static MainMenu instance = null;
-        private Entity backGroundImage;
+        private Entity backGroundImage = new Entity(new StationaryShape(0.0f, 0.0f, 1.0f, 1.0f), new Image("Assets/Images/TitleImage.png"));
         private Text[] menuButtons;
         private int activeMenuButton;
         private int maxMenuButtons;
+
+        //Initialize the menu buttons
+        Text newGameButton = new Text("New Game", new Vec2F(0.3f, 0.3f), new Vec2F(0.5f, 0.5f));
+        Text Quit = new Text("Quit", new Vec2F(0.3f, 0.3f), new Vec2F(0.5f, 0.5f));
+
         public static MainMenu GetInstance() {
             if (MainMenu.instance == null) {
                 MainMenu.instance = new MainMenu();
@@ -15,33 +23,12 @@ namespace Galaga.GalagaStates {
             return MainMenu.instance;
         }
 
-        void ResetState();
-        void UpdateState();
- 
-        void RenderState(){
-            new backGroundImage(TitleImage.png);
-            new menuButtons("New Game", "Quit");
-        }
-    
-        void HandleKeyEvent(KeyboardAction action, KeyboardKey key){
-            //action = "KEY_PRESS";
-            //action = "KEY_RELEASE";
-
-            //KEY_UP
-            //KEY_DOWN
-            //KEY_ENTER
-
-            if(menuButtons.X){
-                GalagaBus.GetBus().RegisterEvent(
-                    new GameEvent{
-                        EventType = GameEventType.GameStateEvent,
-                        Message = "CHANGE_STATE",
-                        StringArg1 = "GAME_RUNNING"
-                    }
-                );
-
+        //Render the titile image and the menu buttons
+        public void RenderState() {
+            backGroundImage.RenderEntity();
+            foreach (Text button in menuButtons) {
+                button.RenderText();
             }
         }
-    }   
-
+    }
 }
