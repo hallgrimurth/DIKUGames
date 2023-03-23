7fbf25a8884598fe3603b93b3a1214fc8a118581
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-23 13:39:40 +0100

    adding pause and switch state

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -5,12 +5,8 @@ using DIKUArcade.GUI;
 using DIKUArcade.Events;
 
 using System.Collections.Generic;
-using DIKUArcade.Physics;
-using System;
-using Galaga.Squadron;
-using Galaga.MovementStrategy;
 using Galaga.GalagaStates;
-using DIKUArcade.State;
+
 
 
 
@@ -40,22 +36,24 @@ namespace Galaga {
         }
 
         public void ProcessEvent(GameEvent gameEvent) {
-            if (gameEvent.EventType == GameEventType.WindowEvent) {
-                switch (gameEvent.Message) {
-                    case "CLOSE_WINDOW":
-                        System.Console.WriteLine("CLOSE_WINDOW message received in game");
-                        window.CloseWindow();
-                        break;
-                }
-            if (gameEvent.EventType == GameEventType.GameStateEvent){
-                switch (gameEvent.Message) {
-                    case "CHANGE_STATE":
-                        System.Console.WriteLine("CHANGE_STATE message received in game");
-                        break;
-                }
+            
+            
+            switch (gameEvent.EventType) {
+                case GameEventType.WindowEvent:
+                //send message to state machine
+                    window.CloseWindow();
+                    
+                    System.Console.WriteLine("CHANGE_STATE message received in game");
+                    break;
+
+                case GameEventType.GameStateEvent:
+                    stateMachine.ProcessEvent(gameEvent);
+                    window.SetKeyEventHandler(stateMachine.ActiveState.HandleKeyEvent);
+                    break;
             }
+            
  
-            }
+            
         }
 
         public override void Render() {
