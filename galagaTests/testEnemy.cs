5e4262a6da27e3feeb76c91d3b6cd4306099b897
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-24 11:27:36 +0100

    game over state

---
+++ galagaTests/testEnemy.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+//Testing the enemy class
+
+namespace galagaTests
+{
+    public class testEnemy
+    {
+        [Test]
+        public void TestEnemy()
+        {
+            //Arrange
+            var enemy = new Galaga_Exercise_3.GalagaEntities.Enemy(new Galaga_Exercise_3.GalagaEntities.EnemyShape(new DIKUArcade.Math.Vec2F(0.1f, 0.1f), new DIKUArcade.Math.Vec2F(0.1f, 0.1f)), new Galaga_Exercise_3.GalagaEntities.MovementStrategy());
+            //Act
+            enemy.Move();
+            //Assert
+            Assert.AreEqual(enemy.Shape.Position.X, 0.1f);
+            Assert.AreEqual(enemy.Shape.Position.Y, 0.1f);
+        }
+    }
+}
