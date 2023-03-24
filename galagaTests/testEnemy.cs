546ee4467772eab3d6cf8837d5d2dd486f058d36
Hallgrimurth <hallgrimur1998@gmail.com>
2023-03-24 13:13:29 +0100

    state testing

--- galagaTests/testEnemy.cs
+++ galagaTests/testEnemy.cs
@@ -1,36 +1,21 @@
 using NUnit.Framework;
-<<<<<<< HEAD
-namespace galagaTests;
 
-public class TestEnemy {
-    [SetUp]
-    public void Setup()
-    {
-    }
+// //Testing the enemy class
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-=======
-
-//Testing the enemy class
-
-namespace galagaTests
-{
-    public class testEnemy
-    {
-        [Test]
-        public void TestEnemy()
-        {
-            //Arrange
-            var enemy = new Galaga_Exercise_3.GalagaEntities.Enemy(new Galaga_Exercise_3.GalagaEntities.EnemyShape(new DIKUArcade.Math.Vec2F(0.1f, 0.1f), new DIKUArcade.Math.Vec2F(0.1f, 0.1f)), new Galaga_Exercise_3.GalagaEntities.MovementStrategy());
-            //Act
-            enemy.Move();
-            //Assert
-            Assert.AreEqual(enemy.Shape.Position.X, 0.1f);
-            Assert.AreEqual(enemy.Shape.Position.Y, 0.1f);
-        }
->>>>>>> states
-    }
-}
+// namespace galagaTests
+// {
+//     public class testEnemy
+//     {
+//         [Test]
+//         public void TestEnemy()
+//         {
+//             //Arrange
+//             var enemy = new Galaga_Exercise_3.GalagaEntities.Enemy(new Galaga_Exercise_3.GalagaEntities.EnemyShape(new DIKUArcade.Math.Vec2F(0.1f, 0.1f), new DIKUArcade.Math.Vec2F(0.1f, 0.1f)), new Galaga_Exercise_3.GalagaEntities.MovementStrategy());
+//             //Act
+//             enemy.Move();
+//             //Assert
+//             Assert.AreEqual(enemy.Shape.Position.X, 0.1f);
+//             Assert.AreEqual(enemy.Shape.Position.Y, 0.1f);
+//         }
+//     }
+// }
