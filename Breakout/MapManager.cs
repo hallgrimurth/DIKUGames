8fcf6c0627743f82f417ce5327f65fca9c135fc4
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-04-14 13:55:07 +0200

    setup

--- Breakout/MapManager.cs
+++ Breakout/MapManager.cs
@@ -3,11 +3,44 @@ using System.IO;
 using System.Collections.Generic;
 using DIKUArcade.Entities;
 
-namespace Breakout{
+namespace Breakout{ 
     class MapManager{
-        public MapManager(){
+        private char[,] map;
+        public MapManager(string filePath){
+            LoadMap(filePath);
+        }
 
+        private void LoadMap(string filePath) {
+            if (!File.Exists(filePath)) {
+                throw new FileNotFoundException("The specified file does not exist.");
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int rows = lines.Length;
+            int cols = lines[0].Length;
+
+            map = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    map[row, col] = lines[row][col];
+                }
+            }
+        }
+
+        public void PrintMap() {
+            int rows = map.GetLength(0);
+            int cols = map.GetLength(1);
+
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    Console.Write(map[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
 
     }
 }
+
