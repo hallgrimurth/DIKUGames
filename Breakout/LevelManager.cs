a3195f6caf64e66003fdc288bf3ea25b013b5ecc
Hallgrimurth <hallgrimur1998@gmail.com>
2023-04-18 10:52:03 +0200

    spawning blocks ----unfinished

--- Breakout/LevelManager.cs
+++ Breakout/LevelManager.cs
@@ -41,11 +41,17 @@ namespace Breakout{
             foreach (string line in mapLines)
             {
                 //Add blocks to entity container depending on the char
-                for (int i = 0; i < line.Length; i++) {
-                    // Console.WriteLine(line[i]);
-                    if (line[i] == '#') {
-                        blocks.AddEntity(new Block(new DynamicShape(new Vec2F(0.1f, 0.9f), new Vec2F(0.1f, 0.1f)), new Image(Path.Combine("Assets", "Images", "red-block.png"))));
+                for (int i = 0; i < mapLines.Count; i++) {
+                    for (int j = 0 ; j < mapLines[i].Length; j++) {
+                        if (mapLines[i][j] == '#') {
+                            blocks.AddEntity(new Block(new DynamicShape(new Vec2F(0.05f*j, 0.083f*i), new Vec2F(0.2f, 0.1f)), new Image(Path.Combine("Assets", "Images", "red-block.png"))));
+                        }
                     }
+                    // Console.WriteLine(mapLines[i]);
+                    // Console.WriteLine("eeeeeeeeeeeeeeeeeeeeeeee");
+                    // if (line[i] == '#') {
+                    //     blocks.AddEntity(new Block(new DynamicShape(new Vec2F(0.1f*i, 0.1f), new Vec2F(0.2f, 0.1f)), new Image(Path.Combine("Assets", "Images", "red-block.png"))));
+                    // }
                 }
 
             }
