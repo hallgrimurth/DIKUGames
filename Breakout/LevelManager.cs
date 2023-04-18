271627675995d2771eb24eecaf5fedcef3ac2c35
Hallgrimurth <hallgrimur1998@gmail.com>
2023-04-18 12:08:57 +0200

    map reader

--- Breakout/LevelManager.cs
+++ Breakout/LevelManager.cs
@@ -39,15 +39,33 @@ namespace Breakout{
             var metaLines = lines.SkipWhile(line => !line.StartsWith("Meta")).Skip(1).TakeWhile(line => !line.StartsWith("Meta")).ToList();
             var legendLines = lines.SkipWhile(line => !line.StartsWith("Legend")).Skip(1).TakeWhile(line => !line.StartsWith("Legend")).ToList();
 
+            //Change legend lines to dictionary with the char as key and the image as value
+            Dictionary<char, string> legend = new Dictionary<char, string>();
+            Dictionary<string, string> meta = new Dictionary<string, string>();
+
+            foreach (string line in legendLines) {
+                string[] legendLine = line.Split(' ');
+                legend.Add(legendLine[0][0], legendLine[1]);
+            }
+
+            foreach (string line in metaLines) {
+                string[] metaLine = line.Split(' ');
+                meta.Add(metaLine[0].Remove(metaLine[0].Length-1,1), metaLine[1]);
+            }
+
+            //print legend
+            foreach (KeyValuePair<char, string> entry in legend) {
+                Console.WriteLine(entry.Key + " " + entry.Value);
+            }
+
             // Print the lines between the two instances of "Map"
-            Console.WriteLine("Map lines:");
             foreach (string line in mapLines)
             {
                 //Add blocks to entity container depending on the char
                 for (int i = 0; i < mapLines.Count; i++) {
                     for (int j = 0 ; j < mapLines[i].Length; j++) {
-                        if (mapLines[i][j] == '#') {
-                            blocks.AddEntity(new Block(new DynamicShape(new Vec2F(0.05f*j, 0.083f*i), new Vec2F(0.2f, 0.1f)), new Image(Path.Combine("Assets", "Images", "red-block.png"))));
+                        if (legend.ContainsKey(mapLines[i][j])) {
+                            blocks.AddEntity(new Block(new DynamicShape(new Vec2F(0.05f*j, 0.083f*i), new Vec2F(0.2f, 0.1f)), new Image(Path.Combine("Assets", "Images", legend[mapLines[i][j]]))));
                         }
                     }
                     // Console.WriteLine(mapLines[i]);
@@ -59,19 +77,19 @@ namespace Breakout{
 
             }
 
-            // Print the lines between the two instances of "Meta"
-            Console.WriteLine("\nMeta lines:");
-            foreach (string line in metaLines)
-            {
-                Console.WriteLine(line);
-            }
+            // // Print the lines between the two instances of "Meta"
+            // Console.WriteLine("\nMeta lines:");
+            // foreach (string line in metaLines)
+            // {
+            //     Console.WriteLine(line);
+            // }
             
-            // Print the lines between the two instances of "Legend"
-            Console.WriteLine("\nLegend lines:");
-            foreach (string line in legendLines)
-            {
-                Console.WriteLine(line);
-            }
+            // // Print the lines between the two instances of "Legend"
+            // Console.WriteLine("\nLegend lines:");
+            // foreach (string line in legendLines)
+            // {
+            //     Console.WriteLine(line);
+            // }
 
 
 
