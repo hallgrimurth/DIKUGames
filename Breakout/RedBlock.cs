ba75a630fc950c37a9bee7dfc97beb10a9967969
Knud <gadelamperne@gmail.com>
2023-04-25 22:09:51 +0200

    Block factory skeleton as well as other changes

---
+++ Breakout/RedBlock.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace Breakout;
+public class RedBlock : Block {
+    public RedBlock(DynamicShape Shape, IBaseImage image)
+        : base(Shape, image) {
+            
+        }
+}
