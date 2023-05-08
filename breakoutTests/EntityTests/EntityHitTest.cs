c955ef9d855a04d1edfc15bdbbe5996b60b3e8af
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-05-08 10:46:57 +0200

    changes

--- breakoutTests/EntityTests/EntityHitTest.cs
+++ breakoutTests/EntityTests/EntityHitTest.cs
@@ -1,31 +1,31 @@
-// using System;
-// using Breakout;
-// using DIKUArcade.Math;
+using System;
+using Breakout;
+using DIKUArcade.Math;
 
-// namespace BreakoutTests
-// {
+namespace BreakoutTests
+{
 
-//     [TestFixture]
-//     public class EntityHitTest
-//     {
+    [TestFixture]
+    public class EntityHitTest
+    {
 
-//         private string path;
-//         private LevelManager level;
-//         private Block block;
-//         [SetUp]
-//         public void Setup()
-//         {
-//             //adding enitites
-//             block = new Block(new DynamicShape(new Vec2F(0.5, 0.95f - 0.5), new Vec2F(0.5, 0.5)), new Image(Path.Combine("Assets", "Images", "block.png")));       
+        private string path;
+        private LevelManager level;
+        private Block block;
+        [SetUp]
+        public void Setup()
+        {
+            //adding enitites
+            block = new Block(new DynamicShape(new Vec2F(0.5, 0.95f - 0.5), new Vec2F(0.5, 0.5)), new Image(Path.Combine("Assets", "Images", "block.png")));       
             
-//         }
+        }
 
-//         [Test]
-//         public void TestHit()
-//         {
-//             //Testing if block is hit
-//             block.Hit();
-//             Assert.AreEqual(block.Health, 2);
-//         }
-//     }
-// }
+        [Test]
+        public void TestHit()
+        {
+            //Testing if block is hit
+            block.Hit();
+            Assert.AreEqual(block.Health, 2);
+        }
+    }
+}
