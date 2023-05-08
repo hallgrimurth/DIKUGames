43f7890c173cc298e1b9325d7859e5614f3ecb34
Knud <gadelamperne@gmail.com>
2023-05-08 11:45:17 +0200

    Fixed levelLoader to better handle empty or invalid files

--- Breakout/BlockFactory.cs
+++ Breakout/BlockFactory.cs
@@ -12,13 +12,12 @@ using DIKUArcade.Graphics;
 
 namespace Breakout {
     public class BlockFactory {
-        private Vec2F blockExtent = new Vec2F((1/12.0f), (1/24.0f));
+        private static Vec2F blockExtent = new Vec2F((1/12.0f), (1/24.0f));
 
         public static Block CreateBlock(int i, int j, Image image, char blockType) {
             Vec2F blockPos = new Vec2F(blockExtent.X*j, 1.0f - blockExtent.Y*i - blockExtent.Y);
             DynamicShape blockShape = new DynamicShape(blockPos, blockExtent);
-            // Case for each block type;
-            // Normal block, indestructible block, powerup block, hardened block
+            //Case for each block type. Normal block, indestructible block, powerup block, hardened block
             switch (blockType) {
                 case 'N':
                     return new NormalBlock(blockShape, image);
--- Breakout/LevelManager.cs
+++ Breakout/LevelManager.cs
@@ -16,11 +16,8 @@ namespace Breakout{
         private List<string> mapData = new List<string>();
         private List<string> metaData = new List<string>();
         private List<string> legendData = new List<string>();
-        // private List<string> availableLevels = new List<string>();
         private Dictionary<char, string> metaDict;
         private Dictionary<char, string> legendDict;
-        
-        // private BlockFactory blockFactory;
         
         public EntityContainer<Block> blocks {get;}
         public List<string> MetaData{
@@ -35,11 +32,6 @@ namespace Breakout{
             LoadTextData(filePath);
             LoadMapEntities(mapData);
         }
-
-        // public void LoadMap(string filePath) {
-        //     LoadTextData(filePath);
-        //     LoadMapMetrics(mapLines);
-        // }
 
         //Loading map, meta and legend from file
         public void LoadTextData(string filePath) {
@@ -64,22 +56,18 @@ namespace Breakout{
 
         //Load entities into entity container
         public void LoadMapEntities(List<string> mapLines) {
-            try {
-                float length = mapLines[0].Length;
-                float height = mapLines.Count;
-                // Create the map from the lines
-                // foreach (string line in mapLines) {
-                    //Add blocks to entity container depending on the char
-                for (int i = 0; i < mapLines.Count ; i++) {
-                    for (int j = 0 ; j < length; j++) {
-
+            float length = 12;
+            float height = 24;
+            // Create the map from the lines
+            for (int i = 0; i < height ; i++) {
+                for (int j = 0 ; j < length; j++) {
+                    // Attempts to add a block to the entity container depending on the char
+                    try {
                         if (legendDict.ContainsKey(mapLines[i][j])) {
-                            //check if the char is a value in the meta dictionary if so, set 
-                            //type to that key
                             char type = 'n' ;
-                            //print the key in metadata where the value is the char
 
-                            
+                            // Check if the char is a value in the meta dictionary if so, set 
+                            // type to that key
                             if (metaDict.ContainsValue(mapLines[i][j].ToString())) type = 
                                 metaDict.Where(x => x.Value[0] == 
                                     mapLines[i][j]).Select(x => x.Key).FirstOrDefault();
@@ -87,21 +75,26 @@ namespace Breakout{
                             Image blockImage = new Image(Path.Combine("Assets", "Images", 
                                 legendDict[mapLines[i][j]]));
                             blocks.AddEntity(BlockFactory.CreateBlock(i, j, blockImage, type));
-
                         }
+
+                    // // These catches is needed to avoid crashing if the file is empty or data is missing
+                    // // The program will continue without placing a block
+                    } catch (ArgumentOutOfRangeException) {
+
+                    } catch (IndexOutOfRangeException) {
+
                     }
                 }
-            // }
-            } catch (ArgumentOutOfRangeException) {
-                // This catch is needed to avoid crashing if the file is empty or data is missing
             }
         }
 
+        // Parse a segment of the text file
         private List<string> ParseSegment(string[] lines ,string segment){
             return lines.SkipWhile(line => !line.StartsWith(segment)).Skip(1).TakeWhile(
                 line => !line.StartsWith(segment)).ToList();
         }
 
+        // Create a dictionary from a list of strings
         private Dictionary<char, string> GetDict(List<String> list){
             return list.Select(line => line.Split(' ')).ToDictionary(
                 line => line[0][0], line => line[1]);
