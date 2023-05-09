81f1a007ecdcb6da085511892df21ac7d402de9d
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-05-09 13:55:23 +0200

    ball

--- Breakout/Ball.cs
+++ Breakout/Ball.cs
@@ -6,30 +6,18 @@ using DIKUArcade.Math;
 namespace Breakout;
 public class Ball : Entity {
 
-    private DynamicShape shape;
-    private static Vec2F extent = new Vec2F(0.008f, 0.021f);
-    private static Vec2F direction = new Vec2F(0.0f, 0.05f);
+    // private DynamicShape shape;
+    private static Vec2F extent = new Vec2F(0.1f, 0.1f);
+    private static Vec2F direction = new Vec2F(-0.01f, 0.01f);
     public Vec2F Direction {
         get { return direction; }
+        set { direction = value; }
     }
 
     public Ball(Vec2F pos, IBaseImage image)
         : base(new DynamicShape(pos, extent), image) {
         Shape.AsDynamicShape().Direction = direction;
-        this.shape = new DynamicShape(pos, extent);
+        // this.shape = new DynamicShape(pos, extent);
     }
 
-    public void Move() {
-        shape.Move();
-
-        if(shape.Position.X  < 0.0f) {
-            shape.Position.X = 0.0f;
-        } else if(shape.Position.X > 1.0f - shape.Extent.X) {
-            shape.Position.X = - 1.0f - shape.Extent.X;
-        } else if(shape.Position.Y > 1.0f - shape.Extent.X) {
-            shape.Position.Y = - 1.0f - shape.Extent.Y;
-        } else if(shape.Position.Y > 1.0f - shape.Extent.Y) {
-            shape.Position.Y = - 1.0f - shape.Extent.Y;
-        }
-    }
 }
--- Breakout/BreakoutStates/GameRunning.cs
+++ Breakout/BreakoutStates/GameRunning.cs
@@ -43,23 +43,31 @@ namespace Breakout.BreakoutStates {
             ballCon.AddEntity(new Ball(new Vec2F(pos.X+(ex.X/2), pos.Y+(ex.Y/2)), ballImage)); 
         }
 
-        private void IterateBall() {
+                private void IterateBall() {
             ballCon.Iterate(ball => {
                 ball.Shape.Move(ball.Direction); // Using the Direction property from Ball.cs
                 //ball.BallMove();
 
-                if (ball.Shape.Position.X < 0.0f) {
+                if (ball.Shape.Position.Y + ball.Shape.Extent.Y >= 1.0f) {
+                    ball.Direction = new Vec2F(ball.Direction.X, -ball.Direction.Y);
+                } else if (ball.Shape.Position.X <= 0.0f) {
+                    ball.Direction = new Vec2F(-ball.Direction.X, ball.Direction.Y);
+                } else if (ball.Shape.Position.X + ball.Shape.Extent.X >= 1.0f) {
+                    ball.Direction = new Vec2F(-ball.Direction.X, ball.Direction.Y);
+                }
+
+                if (ball.Shape.Position.Y + ball.Shape.Extent.Y < 0.0f) {
                     ball.DeleteEntity();
                 } else {
                     level.blocks.Iterate(block => {
-                        if (CollisionDetection.Aabb(ball.Shape.AsDynamicShape(), block.Shape)
-                            .Collision) {
+                        if (CollisionDetection.Aabb(ball.Shape.AsDynamicShape(), block.Shape).Collision) {
                             // FIX: Ball should change direction upon collision (not be deleted - only to test whether it works)
                             ball.DeleteEntity();
-                            block.DecreaseHealth();
-                            if (block.Health == 0) {
-                                block.DeleteEntity();
-                            }
+                            // block.DecreaseHealth();
+                            // if (block.Health == 0) {
+                            //     block.DeleteEntity();
+                            // }
+                            // another test
                         }
                     });     
                 }
