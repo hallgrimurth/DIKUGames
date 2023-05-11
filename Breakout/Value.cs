403a2e8aa49a7a8dbc3f1acd26180ce47d114cd2
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-05-11 11:31:22 +0200

    score

--- Breakout/Value.cs
+++ Breakout/Value.cs
@@ -9,18 +9,18 @@ public class Value : IGameEventProcessor {
     private int value;
 
     public Value(Vec2F position, Vec2F extent, int startingvalue) {
+        value = startingvalue;
         display = new Text("Score:" + value.ToString(), position, extent);
         display.SetColor(new Vec3I(0, 255, 255));
         display.SetFontSize(30);
-        value = startingvalue;
     }
 
-    public void AddValue() {
+    private void AddValue() {
         value++;
         display.SetText("Score:" + value.ToString());
     }
 
-    public void FinalValue() {
+    private void FinalValue() {
         display = new Text("GameOver \n you reached \n   level: " + value.ToString(), 
             new Vec2F(0.25f, 0.25f), new Vec2F(0.5f, 0.5f));
         display.SetColor(new Vec3I(0, 255, 255));
