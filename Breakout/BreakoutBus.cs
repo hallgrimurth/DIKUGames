db4c03c4849270ae75bce6c3706caa8f295b758e
nynnesbachelorprojekt <bkf248@alumni.ku.dk>
2023-05-26 15:33:21 +0200

    -

--- Breakout/BreakoutBus.cs
+++ Breakout/BreakoutBus.cs
@@ -1,7 +1,7 @@
 using DIKUArcade.Events;
 
 namespace Breakout {
-    public class BreakoutBus {
+    public static class BreakoutBus {
         private static GameEventBus eventBus;
         public static GameEventBus GetBus() {
             return BreakoutBus.eventBus ?? (BreakoutBus.eventBus = new GameEventBus());
